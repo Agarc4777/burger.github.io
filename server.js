878643486e83ce24530c55a8094de04e40c3dda8
Ignacio Garcia <69227346+Agarc4777@users.noreply.github.com>
2021-01-26 20:28:49 -0600

    added a db folder,scheme and seeds inside it.

---
+++ server.js
@@ -0,0 +1,22 @@
+var express = require("express");
+var exphbs = require("express-handlebars");
+
+var app = express();
+
+var PORT = process.env.PORT || 8080;
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+
+
+
+
+
+
+
+
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
