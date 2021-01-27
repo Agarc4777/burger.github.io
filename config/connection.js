4b4c8118b65f90a0e21d3be416f76eac66776b2a
Ignacio Garcia <69227346+Agarc4777@users.noreply.github.com>
2021-01-26 22:38:59 -0600

    added connection.js file and added config folder

---
+++ config/connection.js
@@ -0,0 +1,18 @@
+var mysql = require('mysql');
+var connection;
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+     host: 'localhost',
+     user: 'root',
+     password: 'B055p0w3r!',
+     database: 'burgers_db',
+    });
+};
+
+connection.connect();
+module.exports = connection;
+
+// https://fathomless-escarpment-65723.herokuapp.com/ | https://git.heroku.com/fathomless-escarpment-65723.git
