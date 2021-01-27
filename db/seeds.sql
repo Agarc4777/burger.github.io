ded63bd64926e323fefec1ab6609a3fe8e8e4682
Ignacio Garcia <69227346+Agarc4777@users.noreply.github.com>
2021-01-26 23:20:40 -0600

    added orm.js file. empty file

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,7 +1,7 @@
 USE burgers_db;
 
-INSERT INTO burgers (burger_name) VALUES ('Grilled Chicken')
+INSERT INTO burgers (burger_name) VALUES ('Grilled Chicken');
 
-INSERT INTO burgers (burger_name) VALUES ('All-American Bacon Burger')
+INSERT INTO burgers (burger_name) VALUES ('All-American Bacon Burger');
 
 INSERT INTO burgers (burger_name) VALUES ('Patty Melt')
--- server.js
+++ server.js
@@ -1,9 +1,16 @@
+// dependancies
 var express = require("express");
 var exphbs = require("express-handlebars");
 
+// ==========================================
+// sets up express app
 var app = express();
-
 var PORT = process.env.PORT || 8080;
+
+// =================================================================
+// parsing data 
+app.use(express.urlencode({ extended: true }));
+app.use(express.json());
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
